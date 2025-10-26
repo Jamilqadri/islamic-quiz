22a3c7c1f5f7f9b2a9478827334f567b0b356af3
Jamilqadri <jamilqadri6710@gmail.com>
2025-10-26 08:51:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -383,7 +383,7 @@ class Quiz {
 
     // Update leaderboard from Google Sheets - WITH NEW URL
     updateLeaderboardFromSheets() {
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbwyCZUHEDKko4JYNbsldOPTKGCMFhRtxXEM7QvZUFUcs_KU48ny2NhEFtrjp-uNIQN0/exec';
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwQe51cu8PEm74hZj7KwQF9ApDRbCR1cWnFs0NeCRq1MEan2ZFgMF7RM7wgULTS_Z0P/exec';
         
         // Show loading
         const leaderboardContainer = document.getElementById('globalLeaderboard');
@@ -536,7 +536,7 @@ Can you beat my score?
 
     // Send data to Google Sheets - WITH NEW URL
     sendToGoogleSheets(quizData) {
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbwyCZUHEDKko4JYNbsldOPTKGCMFhRtxXEM7QvZUFUcs_KU48ny2NhEFtrjp-uNIQN0/exec';
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbwQe51cu8PEm74hZj7KwQF9ApDRbCR1cWnFs0NeCRq1MEan2ZFgMF7RM7wgULTS_Z0P/exec';
         
         const params = new URLSearchParams({
             'name': quizData.name,
